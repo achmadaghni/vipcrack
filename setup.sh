f30e8168a50183b052bc87d11be1fb7a1d05f256
Vins id <64899189+MRV1N5@users.noreply.github.com>
2020-06-04 23:41:15 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,3 @@
+z="
+";Rz='mail';Jz='pip2';Qz=' yag';Gz='AHAN';Cz='echo';Kz=' ins';Uz='L "';Az='clea';Sz=' BER';Lz='tall';Tz='HASI';Nz='pgra';Iz='. "';Mz=' --u';Ez='NSTA';Pz='ip';Oz='de p';Bz='r';Dz=' " I';Fz='LL B';Hz=' ...';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Oz$Pz$z$Jz$Kz$Lz$Qz$Rz$z$Cz$Dz$Ez$Fz$Gz$Sz$Tz$Uz"
