5e4351f337bf4e02d98108b85bc0863180d7eb5f
Vins id <64899189+MRV1N5@users.noreply.github.com>
2020-06-04 23:04:37 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+clear
+echo " INSTALL BAHAN .... "
+sleep 3
+echo " Update BAHAN "
+pip2 install --upgrade pip
+sleep 2
+pip2 install yagmail
+sleep 3
+echo " Updating Script Berhasil "
